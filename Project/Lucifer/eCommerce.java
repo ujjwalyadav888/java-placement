d1f4915002d244e4c10d475c9ead1643597ea6aa
ujjwal yadav <yujjwal620@gmail.com>
2026-05-07 12:30:18 +0530

    Add product, cart, and eCommerce classes with basic functionality

--- Project/Lucifer/eCommerce.java
+++ Project/Lucifer/eCommerce.java
@@ -1,18 +1,38 @@
-public class payment{
+import java.util.*;
 
+class product{
+    int id;
+    String productName;
+    int price;
+   
+    product(int id, String productName, int price) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+    }
+    
+     void show() {
+        System.out.println(id + " " + productName + " " + price);
+    }
 }
-public class product{
+
+class cart extends product{
+
+    cart(int id, String productName, int price) {
+        super(id, productName, price);   
+    }  
+    @Override
+    void show() {
+        System.out.println("Product in Cart:");
+        System.out.println(id + " " + productName + " " + price);
+    }
     
 }
-public class Order{
 
-}
-public class cart{
-
-}
-public class user{
-
-}
 public class eCommerce {
-    
+    public static void main(String [] args){
+        product p1= new product(1,"leptop",55000);
+        p1.show();
+       
+    }
 }
